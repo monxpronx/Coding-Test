4ecdf96cc5b5e03fb221f1ec9cf5c6dc7caebf5a
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-04-14 15:48:09 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,11 +1,7 @@
-// 250410
+// 250414
 
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = -1;
-        
-        answer = num1%num2;
-        
-        return answer;
+         return num1%num2;
     }
 }
