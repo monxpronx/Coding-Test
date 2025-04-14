3106184274ce6f21d4c7120345b08221a7031eed
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-04-14 15:53:46 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,14 +1,15 @@
-// 250410
+// 250411
 
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        double ans;
         
-        ans = ((double)num1/num2)*1000;
-        answer = (int)ans;
+        double ans1;
+        int ans2;
         
-        return answer;
+        ans1 = ((double)num1/num2)*1000;
+        ans2 = (int)ans1;
+        
+        return ans2;
     }
 }
 
