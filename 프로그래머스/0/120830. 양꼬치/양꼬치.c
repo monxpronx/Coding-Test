4cf72e3e22d95635b0b44f3c50dcff90992c30d2
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-04-14 15:29:48 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.c
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -3,11 +3,10 @@
 #include <stdlib.h>
 
 int solution(int n, int k) {
-    int answer = 0;
-    int cnt;
-    cnt = k - n/10;
-    answer += 12000*n + 2000*cnt;
     
+    int answer = 0;
+    int cnt = k - n/10; // 계산에 포함되는 양꼬치개수
+    answer += 12000*n + 2000*cnt;
     
     return answer;
 }
