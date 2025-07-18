0c5b25f45fbd89f557166bc85c9bb4edfe10d926
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-07-18 20:53:35 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+// 250718
+
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        answer = 2 * message.length();
+        
+        return answer;
+    }
+}
