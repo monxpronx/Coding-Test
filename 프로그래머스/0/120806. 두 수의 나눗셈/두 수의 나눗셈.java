0620f9528920cbc91096b725d0614d8689eb7b98
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-07-18 16:47:53 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,17 +1,9 @@
-// 250411
+// 250718
 
 class Solution {
     public int solution(int num1, int num2) {
+        int answer = (int) (((double)num1/(double)num2) * 1000);
         
-        double ans1;
-        int ans2;
-        
-        ans1 = ((double)num1/num2)*1000;
-        ans2 = (int)ans1;
-        
-        return ans2;
+        return answer;
     }
 }
-
-// int형 변수 앞에 형변환연산자 (double)을 붙여 소수형으로 계산
-// double형 변수 앞에 형변환연산자 (int)를 붙여 소수점 버리기
