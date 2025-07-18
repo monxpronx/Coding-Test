5260d40b0e3facc34ad11e83bc2797e61676f910
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-07-18 21:38:58 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+// 250718
+
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        
+        answer = n;
+        for(int i=0; i<t; i++) answer *= 2;
+        
+        return answer;
+    }
+}
