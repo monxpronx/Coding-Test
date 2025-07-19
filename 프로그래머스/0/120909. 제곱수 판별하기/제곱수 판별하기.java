e3e55d2a1a4b3e6a9860262440a601dd6b3bc089
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-07-19 17:51:40 +0900

    [level 0] Title: 제곱수 판별하기, Time: 2.06 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,17 @@
+// 250719
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        answer=2;
+        for(int i=1; i<n/2; i++) {
+            if (i*i==n) {
+                answer=1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
