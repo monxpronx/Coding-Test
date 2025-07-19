0bba1c8031dba236b574dd3d7a0f08e265c202f3
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-07-19 13:47:28 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,16 @@
+// 250719
+
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        for(int i=1; ; i++) {
+            if (slice*i >= n) {
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
