b6e57579c619cf25e96077dc824df11351ecac14
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-09-18 11:45:30 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,40 @@
 from collections import deque
 
-def bfs(board, start):
+def bfs(town, start):
     q = deque()
     q.append(start)
-    board[start[0]][start[1]] = 0   # 방문 표시
+    town[start[0]][start[1]] = 0   # 방문 표시 (방문하면 1->0으로 바꿀것)
     cnt = 1
 
     moves = [(1,0),(-1,0),(0,1),(0,-1)]  # 방향(상하좌우)
 
     while q:
-        x, y = q.popleft()
+        curr_x, curr_y = q.popleft()
+
         for dx, dy in moves:
-            nx, ny = x+dx, y+dy
-            if nx < 0 or ny < 0 or nx >= len(board) or ny >= len(board):
+            next_x, next_y = curr_x+dx, curr_y+dy
+            if next_x < 0 or next_y < 0 or next_x >= len(town) or next_y >= len(town):  # 지도 밖으로 나가버리는 경우
                 continue
-            if board[nx][ny] == 1:
-                board[nx][ny] = 0
-                q.append((nx,ny))
+            if town[next_x][next_y] == 1:
+                town[next_x][next_y] = 0  # 방문표시
+                q.append((next_x,next_y))  # 집 추가요
                 cnt += 1
     return cnt
 
 
 if __name__ == "__main__":
+
     N = int(input())
-    board = []
+    town = []
     for _ in range(N):
         row = list(map(int, input().strip()))
-        board.append(row)
+        town.append(row)
 
     ans = []
     for i in range(N):
         for j in range(N):
-            if board[i][j] == 1:
-                ans.append(bfs(board, (i,j)))
+            if town[i][j] == 1:
+                ans.append(bfs(town, (i,j)))  # 방문하지않은 집(1) 발견하면 BFS수행
 
     print(len(ans))
     for x in sorted(ans):
