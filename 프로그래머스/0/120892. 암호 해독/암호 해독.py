741420ea75568c904cd18d91fa3117857273d21c
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-11-22 18:16:29 +0900

    [level 0] Title: 암호 해독, Time: 0.26 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -0,0 +1,10 @@
+# 251122
+
+def solution(cipher, code):
+    answer = ''
+    
+    for i in range(len(cipher)):
+        if (i+1) % code == 0:
+            answer+=cipher[i]
+    
+    return answer
