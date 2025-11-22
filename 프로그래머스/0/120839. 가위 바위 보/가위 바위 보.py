0cd68d24ca3e9fdc55a866c7cd1853f8bf559691
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-11-22 17:53:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,14 @@
+# 251122
+
+def solution(rsp):
+    answer = ''
+    
+    for i in range(len(rsp)):
+        if rsp[i]=='0':
+            answer+='5'
+        elif rsp[i]=='2':
+            answer+='0'
+        else:
+            answer+='2'
+    
+    return answer
