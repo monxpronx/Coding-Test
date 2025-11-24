8d96cfe289ba82d12667fd383b1e6f79edcee4b6
monxpronx <163402903+monxpronx@users.noreply.github.com>
2025-11-24 17:30:23 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.18 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -0,0 +1,16 @@
+# 251124
+
+def solution(s):
+    answer = ''
+    
+    for ch1 in s: # 비교기준문자: ch1
+        cnt=0
+        for ch2 in s: # 비교대상문자: ch2
+            if ch1==ch2:
+                cnt+=1
+        if cnt==1:
+            answer+=ch1
+    
+    answer = ''.join(sorted(answer))
+    
+    return answer
