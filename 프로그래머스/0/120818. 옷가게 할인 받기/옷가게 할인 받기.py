2773443cd3df5e2292362594721b173d6ec02f44
monxpronx <163402903+monxpronx@users.noreply.github.com>
2026-01-09 16:20:31 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,17 @@
+# 260109
+
+def solution(price):
+    answer = 0
+    
+    if price >= 500000:
+        answer = price * 0.8
+    elif price >= 300000:
+        answer = price * 0.9
+    elif price >= 100000:
+        answer = price * 0.95
+    else:
+        answer = price
+        
+    answer = int(answer)
+    
+    return answer
