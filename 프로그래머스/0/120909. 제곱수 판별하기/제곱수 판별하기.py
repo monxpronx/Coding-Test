ac93626583d65e89e74023720582793e7b85cd25
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-02-09 12:56:18 +0900

    [level 0] Title: 제곱수 판별하기, Time: 21.21 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -0,0 +1,13 @@
+# 260209
+
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n+1):
+        if i*i== n:
+            answer = 1
+            break
+        else:
+            answer = 2
+    
+    return answer
