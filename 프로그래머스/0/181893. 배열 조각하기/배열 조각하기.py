2dc5043121ec046544a30f3ae468636a67c8793e
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-03-12 17:57:37 +0900

    [level 0] Title: 배열 조각하기, Time: 1.32 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -0,0 +1,12 @@
+# 260312
+
+def solution(arr, query):
+    answer = arr
+    
+    for i in range(len(query)):
+        if i%2==0:
+            answer = answer[:query[i]+1]
+        else:
+            answer = answer[query[i]:]
+    
+    return answer
