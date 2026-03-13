eeea707100de468abfa6ab29a86105a4b5f780a0
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-03-13 16:44:44 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 152.36 ms, Memory: 40 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,14 @@
+# 260313
+
+def solution(arr):
+    answer = []
+    
+    for i in range(len(arr)):
+        if (50 <= arr[i]) and (arr[i]%2==0):
+            arr[i] /= 2
+        elif (arr[i] <= 50) and (arr[i]%2==1):
+            arr[i] *= 2
+            
+    answer = arr
+    
+    return answer
