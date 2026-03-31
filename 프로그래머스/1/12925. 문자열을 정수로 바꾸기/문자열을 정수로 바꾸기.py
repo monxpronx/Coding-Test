419f03e3ca9996d83f1a915cbce6ec31105305f6
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-03-31 23:56:13 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -0,0 +1,13 @@
+# 260331
+
+def solution(s):
+    answer = 0
+    
+    if s[0] == '+':
+        answer = int(s[1:])
+    elif s[0] == '-':
+        answer = int(s[1:]) * (-1)
+    else:
+        answer = int(s)
+        
+    return answer
