3a22131a0e1e5c283b52995e5e7d740aaa31f73e
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-04-19 20:05:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,12 @@
+# 260419
+
+def solution(numbers):
+    answer = 0
+    
+    num_list = [0,1,2,3,4,5,6,7,8,9]
+    
+    for num in num_list:
+        if num not in numbers:
+            answer += num
+    
+    return answer
