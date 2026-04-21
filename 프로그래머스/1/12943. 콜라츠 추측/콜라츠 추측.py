30bd3f131a66e14e1ff5fc8332caf8682b5bb38d
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-04-21 15:55:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,15 @@
+# 260421
+
+def solution(num):
+    answer = 0
+    
+    while num != 1 and answer < 500:
+        if num%2==0:
+            num = num//2
+        else:
+            num = num*3 + 1
+        answer+=1
+    if num != 1:
+        answer = -1
+    
+    return answer
