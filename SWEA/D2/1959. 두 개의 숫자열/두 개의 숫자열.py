883e9be4dc603fe86774fb6d12f57bdace2e895b
권준형 <163402903+monxpronx@users.noreply.github.com>
2026-07-04 16:40:16 +0900

    [D2] Title: 두 개의 숫자열, Time: 61 ms, Memory: 52,864 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,24 +1,24 @@
-# 260519
+# 260704
 
 T = int(input())
 
 for t in range(1, T+1):
 
     N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
+    A = list(map(int, input().split())) # 길이 N
+    B = list(map(int, input().split())) # 길이 M
 
-    if N < M:
-        big, small = B, A
+    if len(A) > len(B):
+        long_arr, short_arr = A, B
     else:
-        big, small = A, B
+        long_arr, short_arr = B, A
 
     max_value = 0
-    for i in range(len(big)-len(small)+1):
+    for i in range(len(long_arr)-len(short_arr)+1): # 각 시작점 i
         value = 0
-        for j in range(len(small)):
-            value += (small[j] * big[i+j])
+        for j in range(len(short_arr)):
+            value += (short_arr[j] * long_arr[i+j]) 
         if max_value < value:
             max_value = value
-    
+
     print("#{} {}".format(t, max_value))
